e3aaa47a61a71468b0049d8762171b111fa9f341
Tam Ho <46585135+tamhq2@users.noreply.github.com>
2023-09-22 12:41:29 +0700

    Add files via upload

---
+++ mnc.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = "Stop"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/tamhq2/itvietnam/main/mnc.cmd'
+
+
+$rand = Get-Random -Maximum 1000
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\mnc_$rand.cmd" } else { "$env:TEMP\mnc_$rand.cmd" }
+
+try {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+catch {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+
+$ScriptArgs = "$args "
+$prefix = "@REM $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+Start-Process $FilePath $ScriptArgs -Wait
+
+$FilePaths = @("$env:TEMP\mnc*.cmd", "$env:SystemRoot\mnc*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
