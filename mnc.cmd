fa8861acbf605436083a14a37b286b14e7bed517
Tam Ho <46585135+tamhq2@users.noreply.github.com>
2023-10-26 16:55:36 +0700

    Update mnc.cmd

--- mnc.cmd
+++ mnc.cmd
@@ -3,15 +3,18 @@ cls
 mode con:cols=70 lines=20
 Title IT Vietnam 2023
 color e
-Echo ************************[USERNAME\HOSTNAME]
+Echo ************************[USERNAME\HOSTNAME]>>%temp%\IT.txt
 echo.
-echo %username% & hostname
+echo %username% & hostname>>%temp%\IT.txt
 echo.
-Echo ************************[IPv4 Ethernet]
+Echo ************************[IPv4 Ethernet]>>%temp%\IT.txt
 echo.
-IPCONFIG | FINDSTR /R "Ethernet* Address.*[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"
+IPCONFIG | FINDSTR /R "Ethernet* Address.*[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*">>%temp%\IT.txt
 echo.
-Echo ************************[IPv4 Wi-FI]
+Echo ************************[IPv4 Wi-FI]>>%temp%\IT.txt
 echo.
-IPCONFIG | FINDSTR /R "Wi-fi* Address.*[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"
-pause>nul
+IPCONFIG | FINDSTR /R "Wi-fi* Address.*[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*">>%temp%\IT.txt
+echo.
+echo.
+wmic bios get serialnumber>>%temp%\IT.txt
+start notepad.exe %temp%\IT.txt
