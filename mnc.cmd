227ebe68fc7f69fda857d482e54a6c34ededbe7d
Tam Ho <46585135+tamhq2@users.noreply.github.com>
2023-10-26 16:59:53 +0700

    Update mnc.cmd

--- mnc.cmd
+++ mnc.cmd
@@ -3,9 +3,10 @@ cls
 mode con:cols=70 lines=20
 Title IT Vietnam 2023
 color e
-Echo ************************[USERNAME\HOSTNAME]>>%temp%\IT.txt
+Echo ************************[USERNAME\HOSTNAME]>%temp%\IT.txt
 echo.
-echo %username% & hostname>>%temp%\IT.txt
+systeminfo | find /i "System Model">>%temp%\IT.txt
+(echo %username% & hostname) >>%temp%\IT.txt
 echo.
 Echo ************************[IPv4 Ethernet]>>%temp%\IT.txt
 echo.
@@ -16,5 +17,10 @@ echo.
 IPCONFIG | FINDSTR /R "Wi-fi* Address.*[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*">>%temp%\IT.txt
 echo.
 echo.
+echo CARD MANG DA CONNECTED>>%temp%\IT.txt
+getmac | FINDSTR /R "Tcpip* Address.*[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*">>%temp%\IT.txt
+echo.
 wmic bios get serialnumber>>%temp%\IT.txt
 start notepad.exe %temp%\IT.txt
+
+
